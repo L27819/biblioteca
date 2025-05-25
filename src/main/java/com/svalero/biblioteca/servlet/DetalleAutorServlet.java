030436da8e10a4cf6aac06a100e488d7fadb9676
Lucia Felipe <luciafeliperuiz@gmail.com>
2025-05-25 16:05:26 +0200

    vista detalle autores

---
+++ src/main/java/com/svalero/biblioteca/servlet/DetalleAutorServlet.java
@@ -0,0 +1,34 @@
+package com.svalero.biblioteca.servlet;
+
+import com.svalero.biblioteca.dao.AutorDao;
+import com.svalero.biblioteca.database.Database;
+import com.svalero.biblioteca.model.Autor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.Connection;
+
+@WebServlet("/detalle-autor")
+public class DetalleAutorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+
+        try (Connection connection = new Database().getConnection()) {
+            AutorDao autorDao = new AutorDao(connection);
+            Autor autor = autorDao.getById(id);
+
+            request.setAttribute("autor", autor);
+            request.getRequestDispatcher("detalle-autor.jsp").forward(request, response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(500, "Error al cargar el autor.");
+        }
+    }
+}
